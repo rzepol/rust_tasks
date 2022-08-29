0b444bf2c9ad2e97b74de6fda689b7db8a2e26ee
Rob Lopez <relopez@fastmail.com>
2022-08-29 13:53:05 -0500

    Added Send + Sync traits to Task so that it can run on parallel threads.
    Created a DAG scheduler that converts a task into a dag of dependencies.
    With this you can run using the scheduler in single or multi threaded
    modes. Ultimately you could also write a cluster run mode.

---
+++ src/scheduler_ilb.rs
@@ -0,0 +1,322 @@
+/// Module to handle task schdeuling. Represent a taks as a DAG and run tasks from the leafs
+/// through to the head of the tree. This should allow different forms of running including
+/// local, local parallel, and distributed (e.g., to a compute cluster).
+pub mod scheduler {
+    use std::{
+        collections::{HashMap, HashSet},
+        fmt,
+    };
+
+    use crate::tasks::Task;
+    use anyhow::Result;
+    use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+    use uuid::Uuid;
+
+    /// Node data for a DAG including an identifier, a task, parent and children ids,
+    /// and done status
+    pub struct Node {
+        pub id: Uuid,
+        pub task: Box<dyn Task>,
+        pub is_done: bool,
+        pub parent: Option<Uuid>,
+        pub children: HashSet<Uuid>,
+    }
+
+    impl fmt::Debug for Node {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            f.debug_struct("Node")
+                .field("id", &self.id)
+                .field("task", &self.task.get_name())
+                .field("is_done", &self.is_done)
+                .field("parent", &self.parent)
+                .field("children", &self.children)
+                .finish()
+        }
+    }
+
+    struct ChildData {
+        id: Uuid,
+        task: Box<dyn Task>,
+        parent: Uuid,
+    }
+
+    struct NodeWithChildren {
+        node: Node,
+        children: Vec<ChildData>,
+    }
+
+    pub enum RunStyle {
+        LOCAL,
+        PARALLEL,
+        // CLUSTER
+    }
+
+    #[derive(Debug)]
+    pub struct DAG {
+        pub nodes: HashMap<Uuid, Node>,
+    }
+
+    impl DAG {
+        pub fn new(head_task: Box<dyn Task>) -> Result<Self> {
+            let mut to_process = Vec::new();
+            let mut processed = HashMap::new();
+            let node_data = DAG::make_node(head_task, None, Uuid::new_v4())?;
+            processed.insert(node_data.node.id, node_data.node);
+            to_process.extend(node_data.children);
+
+            while let Some(child_data) = to_process.pop() {
+                let node_data =
+                    DAG::make_node(child_data.task, Some(child_data.parent), child_data.id)?;
+                processed.insert(node_data.node.id, node_data.node);
+                to_process.extend(node_data.children);
+            }
+
+            Ok(Self { nodes: processed })
+        }
+
+        // Run all tasks in the DAG
+        pub fn run(&mut self, run_style: RunStyle) -> Result<()> {
+            let mut finished = self
+                .nodes
+                .values()
+                .filter(|&node| node.is_done)
+                .map(|node| node.id)
+                .collect::<HashSet<_>>();
+            let mut not_finished = self
+                .nodes
+                .values()
+                .filter(|&node| !node.is_done)
+                .map(|node| node.id)
+                .collect::<HashSet<_>>();
+
+            while !&not_finished.is_empty() {
+                let candidate_ids = self.get_run_candidates(&not_finished);
+                match run_style {
+                    RunStyle::LOCAL => {
+                        candidate_ids.clone().into_iter().for_each(|id| {
+                            if let Some(node) = self.nodes.get(&id) {
+                                let _ = node.task.run_no_deps().is_ok();
+                            }
+                        });
+                    }
+                    RunStyle::PARALLEL => {
+                        candidate_ids.clone().into_par_iter().for_each(|id| {
+                            if let Some(node) = self.nodes.get(&id) {
+                                let _ = node.task.run_no_deps().is_ok();
+                            }
+                        });
+                    }
+                };
+                for id in candidate_ids {
+                    if let Some(node) = self.nodes.get_mut(&id) {
+                        node.is_done = true;
+                        finished.insert(id);
+                        not_finished.remove(&id);
+                    }
+                }
+            }
+            Ok(())
+        }
+
+        // return run candidates: nodes that are not already done and where the children are all done
+        // (i.e., the dependencies are all satisfied)
+        fn get_run_candidates(&self, not_finished: &HashSet<Uuid>) -> HashSet<Uuid> {
+            let mut candidates = HashSet::new();
+            for id in not_finished {
+                if let Some(node) = self.nodes.get(id) {
+                    if !node.is_done && node.children.intersection(not_finished).next().is_none() {
+                        candidates.insert(*id);
+                    }
+                }
+            }
+            candidates
+        }
+
+        fn make_node(
+            task: Box<dyn Task>,
+            parent_id: Option<Uuid>,
+            node_id: Uuid,
+        ) -> Result<NodeWithChildren> {
+            let is_done = task.get_target()?.exists();
+            let dep_tasks = task.get_dep_tasks();
+            let child_tasks = dep_tasks.into_values().collect::<Vec<_>>();
+            let mut children = Vec::new();
+            for child in child_tasks {
+                children.push(ChildData {
+                    id: Uuid::new_v4(),
+                    task: child,
+                    parent: node_id,
+                });
+            }
+            let node = Node {
+                id: node_id,
+                task,
+                is_done,
+                parent: parent_id,
+                children: children.iter().map(|c| c.id).collect::<HashSet<_>>(),
+            };
+            Ok(NodeWithChildren { node, children })
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use std::collections::HashMap;
+
+        use crate::{
+            scheduler::DAG,
+            tasks::{FileTarget, Target, Task},
+        };
+        use anyhow::Result;
+
+        #[derive(Debug)]
+        struct Dep1 {}
+        impl Task for Dep1 {
+            fn get_name(&self) -> String {
+                "Dep1".to_string()
+            }
+
+            fn get_target(&self) -> Result<Box<dyn Target>> {
+                Ok(Box::new(FileTarget {
+                    cache_dir: "/tmp".to_string(),
+                    local_filename: "test_dag_target_dep1.txt".to_string(),
+                }))
+            }
+
+            fn get_data(&self) -> Result<Vec<u8>> {
+                Ok("dep1 data".as_bytes().to_vec())
+            }
+        }
+
+        #[derive(Debug)]
+        struct Dep2 {}
+        impl Task for Dep2 {
+            fn get_name(&self) -> String {
+                "Dep2".to_string()
+            }
+
+            fn get_target(&self) -> Result<Box<dyn Target>> {
+                Ok(Box::new(FileTarget {
+                    cache_dir: "/tmp".to_string(),
+                    local_filename: "test_dag_target_dep2.txt".to_string(),
+                }))
+            }
+
+            fn get_dep_tasks(&self) -> HashMap<String, Box<dyn Task>> {
+                let mut result = HashMap::<String, Box<dyn Task>>::new();
+                result.insert("dep3".to_string(), Box::new(Dep3 {}));
+                result
+            }
+
+            fn get_data(&self) -> Result<Vec<u8>> {
+                let dep_targets = self
+                    .get_dep_targets()
+                    .expect("Couldn't get dependent targets");
+                let mut s1 = dep_targets.get("dep3").unwrap().read()?;
+                s1.extend(" - dep2".as_bytes());
+                Ok(s1)
+            }
+        }
+
+        #[derive(Debug)]
+        struct Dep3 {}
+        impl Task for Dep3 {
+            fn get_name(&self) -> String {
+                "Dep3".to_string()
+            }
+
+            fn get_target(&self) -> Result<Box<dyn Target>> {
+                Ok(Box::new(FileTarget {
+                    cache_dir: "/tmp".to_string(),
+                    local_filename: "test_dag_target_dep3.txt".to_string(),
+                }))
+            }
+
+            fn get_data(&self) -> Result<Vec<u8>> {
+                Ok("dep3 data".as_bytes().to_vec())
+            }
+        }
+
+        #[derive(Debug)]
+        struct FinalTask {}
+        impl Task for FinalTask {
+            fn get_name(&self) -> String {
+                "FinalTask".to_string()
+            }
+
+            fn get_target(&self) -> Result<Box<dyn Target>> {
+                Ok(Box::new(FileTarget::new(
+                    "/tmp",
+                    "test_dag_target_depfinal.txt",
+                )))
+            }
+
+            fn get_dep_tasks(&self) -> HashMap<String, Box<dyn Task>> {
+                let mut result = HashMap::<String, Box<dyn Task>>::new();
+                result.insert("dep1".to_string(), Box::new(Dep1 {}));
+                result.insert("dep2".to_string(), Box::new(Dep2 {}));
+                result
+            }
+
+            fn get_data(&self) -> Result<Vec<u8>> {
+                let dep_targets = self
+                    .get_dep_targets()
+                    .expect("Couldn't get dependent targets");
+                let mut s1 = dep_targets.get("dep1").unwrap().read()?;
+                s1.extend(" - ".as_bytes());
+                s1.extend(dep_targets.get("dep2").unwrap().read()?);
+                Ok(s1)
+            }
+        }
+
+        #[test]
+        fn construct_scheduler() {
+            let task: Box<dyn Task> = Box::new(FinalTask {});
+            task.recursively_delete_data()
+                .expect("Failed to delete task and dependent task data");
+            let dag = DAG::new(task).expect("Failed to construct DAG");
+            let any_done = dag.nodes.values().any(|node| node.is_done);
+            assert!(!any_done);
+
+            let task: Box<dyn Task> = Box::new(FinalTask {});
+            task.run().expect("task failed to run");
+            let dag = DAG::new(task).expect("Failed to construct DAG");
+            let all_done = dag.nodes.values().all(|node| node.is_done);
+            assert!(all_done);
+        }
+
+        #[test]
+        fn local_run() {
+            let task: Box<dyn Task> = Box::new(FinalTask {});
+            task.recursively_delete_data()
+                .expect("Failed to delete task and dependent task data");
+            let mut dag = DAG::new(task).expect("Failed to construct DAG");
+
+            let any_done = dag.nodes.values().any(|node| node.is_done);
+            assert!(!any_done);
+
+            dag.run(crate::scheduler::RunStyle::LOCAL)
+                .expect("Failed to run the DAG");
+
+            let all_done = dag.nodes.values().all(|node| node.is_done);
+            assert!(all_done);
+        }
+
+        #[test]
+        fn parallel_run() {
+            let task: Box<dyn Task> = Box::new(FinalTask {});
+            task.recursively_delete_data()
+                .expect("Failed to delete task and dependent task data");
+            let mut dag = DAG::new(task).expect("Failed to construct DAG");
+
+            let any_done = dag.nodes.values().any(|node| node.is_done);
+            assert!(!any_done);
+
+            dag.run(crate::scheduler::RunStyle::PARALLEL)
+                .expect("Failed to run the DAG");
+
+            let all_done = dag.nodes.values().all(|node| node.is_done);
+            assert!(all_done);
+        }
+    }
+}
---
+++ src/task_lib.rs
@@ -0,0 +1,422 @@
+pub mod tasks {
+    use anyhow::Result;
+    use std::{collections::HashMap, fmt, fs, path};
+
+    /// The Target trait represents cached data. The data is stored as a byte slice, and can be used
+    /// with serde for serialization of other types.
+    pub trait Target {
+        /// Read data from a cache
+        fn read(&self) -> Result<Vec<u8>>;
+
+        /// Write data to a cache
+        fn write(&self, s: &[u8]) -> Result<()>;
+
+        /// Delete cache data
+        fn delete(&self) -> Result<()>;
+
+        /// Does the cache exist
+        fn exists(&self) -> bool;
+    }
+
+    /// The FileTarget type implements Target, using a file as the cache destination.
+    #[derive(Debug, PartialEq, Eq)]
+    pub struct FileTarget {
+        pub cache_dir: String,
+        pub local_filename: String,
+    }
+
+    impl FileTarget {
+        pub fn new(cache_dir: &str, local_filename: &str) -> Self {
+            FileTarget {
+                cache_dir: cache_dir.to_string(),
+                local_filename: local_filename.to_string(),
+            }
+        }
+
+        /// Cache filename
+        pub fn filename(&self) -> path::PathBuf {
+            path::Path::new(self.cache_dir.as_str()).join(self.local_filename.as_str())
+        }
+    }
+
+    /// The implementation just uses std::fs file operations.
+    impl Target for FileTarget {
+        fn read(&self) -> Result<Vec<u8>> {
+            Ok(fs::read(&self.filename())?)
+        }
+
+        fn write(&self, s: &[u8]) -> Result<()> {
+            Ok(fs::write(self.filename(), s)?)
+        }
+
+        fn exists(&self) -> bool {
+            self.filename().is_file()
+        }
+
+        fn delete(&self) -> Result<()> {
+            if self.exists() {
+                Ok(fs::remove_file(self.filename())?)
+            } else {
+                Ok(())
+            }
+        }
+    }
+
+    #[derive(Debug, PartialEq, Eq)]
+    pub struct DatedFileTarget {
+        file_target: FileTarget,
+        date: chrono::NaiveDate,
+    }
+
+    impl DatedFileTarget {
+        pub fn new(cache_dir: &str, local_filename: &str, date: chrono::NaiveDate) -> Self {
+            let file_target = FileTarget {
+                cache_dir: cache_dir.to_string(),
+                local_filename: local_filename.to_string(),
+            };
+            DatedFileTarget { file_target, date }
+        }
+
+        fn filename(&self) -> path::PathBuf {
+            let dstr = self.date.format("%Y%m%d").to_string();
+            let local_filename = format!("{}_{}", dstr, self.file_target.local_filename);
+            path::Path::new(self.file_target.cache_dir.as_str()).join(local_filename)
+        }
+    }
+
+    // TODO: bad code smell - this implementation is the same as for FileTarget - investigate how to fix
+    impl Target for DatedFileTarget {
+        fn read(&self) -> Result<Vec<u8>> {
+            Ok(fs::read(&self.filename())?)
+        }
+
+        fn write(&self, s: &[u8]) -> Result<()> {
+            Ok(fs::write(self.filename(), s)?)
+        }
+
+        fn exists(&self) -> bool {
+            self.filename().is_file()
+        }
+
+        fn delete(&self) -> Result<()> {
+            if self.exists() {
+                Ok(fs::remove_file(self.filename())?)
+            } else {
+                Ok(())
+            }
+        }
+    }
+
+    /// The Task trait represents a piece of work with optional Task dependencies. This is modeled
+    /// after the python luigi module.
+    pub trait Task: fmt::Debug + Sync + Send {
+        /// Target for task output
+        fn get_target(&self) -> Result<Box<dyn Target>>;
+
+        /// The result of the task. This can use dependent task data as we will ensure that these
+        /// have been run.
+        /// Don't call this directly unless you want to bypass the cache system
+        fn get_data(&self) -> Result<Vec<u8>>;
+
+        fn get_name(&self) -> String {
+            "Unimplemented".to_string()
+        }
+
+        /// Dependencies, stored in a HashMap. These will be generated using the run method.
+        /// This is like the requires() method in luigi.
+        fn get_dep_tasks(&self) -> HashMap<String, Box<dyn Task>> {
+            HashMap::new()
+        }
+
+        /// Dependent task targets.
+        fn get_dep_targets(&self) -> Result<HashMap<String, Box<dyn Target>>> {
+            let mut result = HashMap::<String, Box<dyn Target>>::new();
+            for (k, task) in self.get_dep_tasks() {
+                result.insert(k, task.get_target()?);
+            }
+            Ok(result)
+        }
+
+        /// This method recursively generates dependent data, and then calls get_data for the Task.
+        fn run(&self) -> Result<()> {
+            // recursively run dependent tasks
+            for (_, dep) in self.get_dep_tasks() {
+                dep.run()?;
+            }
+            let target = self.get_target()?;
+            if !target.exists() {
+                let data = self.get_data()?;
+                target.write(&data)?;
+            }
+            Ok(())
+        }
+
+        /// Non-dependent run: just save get_data() to get_target(). This will fail if required
+        /// dependencies are not present. For regular use just call run(). This method is used in the
+        /// scheduler run method as dependencies are handled in the code there.
+        fn run_no_deps(&self) -> Result<()> {
+            let target = self.get_target()?;
+            if !target.exists() {
+                let data = self.get_data()?;
+                target.write(&data)?;
+            }
+            Ok(())
+        }
+
+        fn delete_data(&self) -> Result<()> {
+            let target = self.get_target()?;
+            target.delete()?;
+            Ok(())
+        }
+
+        /// Non-recursively delete dependencies, i.e., delete task outputs for dependent tasks
+        fn delete_deps(&self) -> Result<()> {
+            for (_, dep) in self.get_dep_targets()? {
+                dep.delete()?;
+            }
+            Ok(())
+        }
+
+        /// Recursively delete dependencies, i.e., delete task outputs for dependent tasks and
+        /// their dependencies as well.
+        fn recursively_delete_data(&self) -> Result<()> {
+            self.delete_data()?;
+            for (_, dep) in self.get_dep_tasks() {
+                dep.recursively_delete_data()?;
+            }
+            Ok(())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    extern crate serde;
+    use anyhow::Result;
+    use serde::{Deserialize, Serialize};
+
+    use crate::tasks::{DatedFileTarget, FileTarget, Target, Task};
+
+    #[test]
+    fn file_target() {
+        let ft = FileTarget::new("/tmp", "test_target.txt");
+        ft.delete().unwrap();
+        assert!(!ft.exists());
+        ft.write("test data".as_bytes()).unwrap();
+        assert!(ft.exists());
+        assert_eq!(ft.read().unwrap(), "test data".as_bytes().to_vec());
+    }
+
+    #[test]
+    fn dated_file_target() {
+        let ft = DatedFileTarget::new(
+            "/tmp",
+            "dated_test_target.txt",
+            chrono::NaiveDate::from_ymd(2021, 9, 3),
+        );
+        ft.delete().unwrap();
+        assert!(!ft.exists());
+        ft.write("test data".as_bytes()).unwrap();
+        assert!(ft.exists());
+        assert_eq!(ft.read().unwrap(), "test data".as_bytes().to_vec());
+    }
+
+    #[test]
+    fn file_task() {
+        #[derive(Debug)]
+        struct FileTask {}
+        impl Task for FileTask {
+            fn get_target(&self) -> Result<Box<dyn Target>> {
+                Ok(Box::new(FileTarget::new("/tmp", "test_task_target.txt")))
+            }
+
+            fn get_data(&self) -> Result<Vec<u8>> {
+                Ok("some data".as_bytes().to_vec())
+            }
+        }
+
+        let task = FileTask {};
+        let target = task.get_target().expect("Can't get target");
+        // test with no starting data
+        target.delete().unwrap();
+        // generate the data
+        task.run().unwrap();
+        assert_eq!(target.read().unwrap(), "some data".as_bytes().to_vec());
+        // test with cached starting data
+        assert_eq!(target.read().unwrap(), "some data".as_bytes().to_vec());
+    }
+
+    #[test]
+    fn serde_task() {
+        #[derive(Debug)]
+        struct FileTask {
+            value: f64,
+        }
+
+        impl FileTask {
+            fn get_value(&self) -> f64 {
+                let v: f64 = serde_json::from_slice(
+                    &self.get_target().expect("Can't get target").read().unwrap(),
+                )
+                .unwrap();
+                v
+            }
+        }
+
+        impl Task for FileTask {
+            fn get_target(&self) -> Result<Box<dyn Target>> {
+                Ok(Box::new(FileTarget::new(
+                    "/tmp",
+                    "test_serde_task_target.txt",
+                )))
+            }
+
+            fn get_data(&self) -> Result<Vec<u8>> {
+                let s = serde_json::to_vec(&self.value).unwrap();
+                Ok(s)
+            }
+        }
+
+        let task = FileTask { value: 1.23 };
+        let target = task.get_target().expect("Can't get target");
+        // test with no starting data
+        target.delete().unwrap();
+        // generate the data
+        task.run().unwrap();
+        assert_eq!(task.get_value(), 1.23);
+    }
+
+    #[test]
+    fn serde_struct_task() {
+        // the thing we want to compute and cache
+        #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+        struct Value {
+            a: String,
+            b: f64,
+        }
+
+        // value is hard-coded here. In a realistic example we'd compute something in get_data()
+        #[derive(Debug)]
+        struct FileTask {
+            value: Value,
+        }
+
+        impl FileTask {
+            fn get_value(&self) -> Value {
+                let v: Value = serde_json::from_slice(
+                    &self.get_target().expect("Can't get target").read().unwrap(),
+                )
+                .unwrap();
+                v
+            }
+        }
+
+        impl Task for FileTask {
+            fn get_target(&self) -> Result<Box<dyn Target>> {
+                Ok(Box::new(FileTarget::new(
+                    "/tmp",
+                    "test_serde_struct_task_target.txt",
+                )))
+            }
+
+            fn get_data(&self) -> Result<Vec<u8>> {
+                let s = serde_json::to_vec(&self.value).unwrap();
+                Ok(s)
+            }
+        }
+
+        let value = Value {
+            a: String::from("a string"),
+            b: 1.23,
+        };
+        let task = FileTask {
+            value: value.clone(),
+        };
+        let target = task.get_target().expect("Can't get target");
+        // test with no starting data
+        target.delete().unwrap();
+        // generate the data
+        task.run().unwrap();
+        let read_value = task.get_value();
+        assert_eq!(value, read_value);
+    }
+
+    #[test]
+    fn dependent_file_task() {
+        #[derive(Debug)]
+        struct Dep1 {}
+        impl Task for Dep1 {
+            fn get_target(&self) -> Result<Box<dyn Target>> {
+                Ok(Box::new(FileTarget {
+                    cache_dir: "/tmp".to_string(),
+                    local_filename: "test_task_target_dep1.txt".to_string(),
+                }))
+            }
+
+            fn get_data(&self) -> Result<Vec<u8>> {
+                Ok("dep1 data".as_bytes().to_vec())
+            }
+        }
+
+        #[derive(Debug)]
+        struct Dep2 {}
+        impl Task for Dep2 {
+            fn get_target(&self) -> Result<Box<dyn Target>> {
+                Ok(Box::new(FileTarget {
+                    cache_dir: "/tmp".to_string(),
+                    local_filename: "test_task_target_dep2.txt".to_string(),
+                }))
+            }
+
+            fn get_data(&self) -> Result<Vec<u8>> {
+                Ok("dep2 data".as_bytes().to_vec())
+            }
+        }
+
+        #[derive(Debug)]
+        struct FinalTask {}
+        impl Task for FinalTask {
+            fn get_target(&self) -> Result<Box<dyn Target>> {
+                Ok(Box::new(FileTarget::new(
+                    "/tmp",
+                    "test_task_target_depfinal.txt",
+                )))
+            }
+
+            fn get_dep_tasks(&self) -> HashMap<String, Box<dyn Task>> {
+                let mut result = HashMap::<String, Box<dyn Task>>::new();
+                result.insert("dep1".to_string(), Box::new(Dep1 {}));
+                result.insert("dep2".to_string(), Box::new(Dep2 {}));
+                result
+            }
+
+            fn get_data(&self) -> Result<Vec<u8>> {
+                let dep_targets = self
+                    .get_dep_targets()
+                    .expect("Couldn't get dependent targets");
+                let mut s1 = dep_targets.get("dep1").unwrap().read()?;
+                s1.extend(" - ".as_bytes());
+                s1.extend(dep_targets.get("dep2").unwrap().read()?);
+                Ok(s1)
+            }
+        }
+
+        let task = FinalTask {};
+        let requires = task.get_dep_tasks();
+        task.recursively_delete_data().unwrap();
+        task.run().unwrap();
+        assert_eq!(
+            requires.get("dep1").unwrap().get_data().unwrap(),
+            "dep1 data".as_bytes().to_vec()
+        );
+        assert_eq!(
+            requires.get("dep2").unwrap().get_data().unwrap(),
+            "dep2 data".as_bytes().to_vec()
+        );
+        assert_eq!(
+            task.get_data().unwrap(),
+            "dep1 data - dep2 data".as_bytes().to_vec()
+        );
+    }
+}
